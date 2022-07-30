6a0cb7b0039e671e51ed66de5bbc1e37a05f8e8e
Bwalk5 <branowalk@gmail.com>
2022-07-30 04:06:28 -0400

    Add files via upload
    This is a reworked and working and cleaner code of Rock Paper Scissors utilizing functions

--- dupjava.js
+++ dupjava.js
@@ -1,39 +1,54 @@
+let myArray = ["rock", "paper", "scissors"];
+const computerChoice = computerOptions();
+const playerChoice = playerSelection();
 
-function play(){
-    let userChoice = prompt("Please select Rock, Paper, Scissors").toLowerCase();
-    let myArray = ["Rock", "Paper", "Scissors"];
-    var computerChoice = myArray[Math.floor(Math.random()*myArray.length)]; 
-    
-    if(userChoice == "rock" && computerChoice == "Scissors"){
-        console.log("The computer played " + computerChoice+ ". You won")
-    
-    }else if (userChoice == "rock" && computerChoice == "Rock") {
-            console.log("The computer played " + computerChoice+ ". You Tied")
-    
-    } else if (userChoice == "rock" && computerChoice == "Paper") {
-        console.log("The computer played " + computerChoice+ ". You Lost")
-    
-    }else if(userChoice == "paper"  && computerChoice == "Rock"){
-        console.log("The computer played " + computerChoice+ ". You won")
-    
-    } else if (userChoice == "paper" && computerChoice == "Paper") {
-            console.log("The computer played " + computerChoice+ ". You Tied")
-    
-    }else if (userChoice == "paper" && computerChoice == "Scissors") {
-        console.log("The computer played " + computerChoice+ ". You Lost")
-    
-    }else if(userChoice == "scissors" && computerChoice == "Paper"){
-        console.log("The computer played " + computerChoice+ ". You won")
-    
-    }else if (userChoice == "scissors" && computerChoice == "Scissors") {
-        console.log("The computer played " + computerChoice+ ". You Tied")
-    
-    }else if (userChoice == "scissors" && computerChoice == "Rock") {
-            console.log("The computer played " + computerChoice+ ". You Lost") 
-    }
+
+function computerOptions() {   
+    var choices = myArray[Math.floor(Math.random()*myArray.length)];
+    return choices.toLowerCase();
 }
 
-for (let i = 0; i < 5; i++) {
-    play();
- }
+function playRound(playerChoice, computerChoice){
+    if((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")){
+    console.log("You Won")
+    }
+   
+    else if(playerChoice == computerChoice){
+        console.log("You Tied")
+    }
+    else
+    console.log("You Lost") 
+}
 
+function playerSelection(){
+    let input = prompt("Type Rock, Paper, or Scissors!")
+    while (input == null){
+        input = prompt("Type Rock, Paper, or Scissors!")
+    }
+    input = input.toLowerCase()
+
+    let check = validateInput(input);
+    while (check == false){
+        input = prompt("Type Rock, Paper, or Scissors! Spelling matters. Capitalization does Notification.")
+        
+        while (input == null){
+            input = prompt("Type Rock, Paper, or Scissors!")
+        }
+
+        input = input.toLowerCase()
+        check = validateInput(input)
+    }
+
+    return input
+    }
+    
+
+
+function validateInput(choice){
+    return myArray.includes(choice);
+
+}
+
+console.log("The computer chose: " + computerChoice)
+console.log("You chose: " + playerChoice)
+playRound(playerChoice, computerChoice);
